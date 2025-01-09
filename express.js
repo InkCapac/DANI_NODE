34ef8f1de7471abe1c35f46ec2181dbadf01c8d1
Matheus Bond <pachamamajunior@gmail.com>
2025-01-09 09:48:27 +0100

    CAMBIOS 09-01
    :)

--- express.js
+++ express.js
@@ -1,6 +1,7 @@
 const express = require ('express');
 const path = require("path");
-
+//Es mejor definir el puerto como un <<const>>
+const puerto = 8080;
 const app = express();
 
 app.get("/", (req, res) => {
@@ -12,4 +13,8 @@ app.get("/contacto", (req, res) => {
 app.use((req, res) => {
     res.sendFile(path.join(__dirname, "404.html"));
 })
-app.listen(8080)
+app.listen(puerto, () => {
+    console.log(`El servidor ha arrancado en el puerto ${puerto}`);
+    //La línea de abajo explica como se hacía antes
+    console.log("El servidor ha arrancado en el puerto "+puerto);
+});
