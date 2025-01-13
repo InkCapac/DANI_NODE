d3d78e2cb2b21f6129bf1d10248fe23a0dbfa53d
Matheus Bond <pachamamajunior@gmail.com>
2025-01-13 11:19:08 +0100

    CAMBIOS 13-01
    :)

--- vite-project2/src/App.tsx
+++ vite-project2/src/App.tsx
@@ -3,15 +3,46 @@ import Header from "./components/Header";
 import Articulo from "./components/Articulo";
 function App() {
 
+const articulos = [
+{
+  titulo: "asas",
+  imagen: "",
+  descripcion: ""
+},
+{
+  titulo: "ddd",
+  imagen: "",
+  descripcion: ""
+},
+{
+  titulo: "ccc",
+  imagen: "",
+  descripcion: ""
+}
+]
   return (
     //En este apartado no se escribe 'class' sino 'className'
     //<div> Puede ser una etiqueta vacía, también
+    //Antigua línea de código
+    /*<Articulo titulo={articulos.titulo} imagen={articulos.imagen} descripcion={articulos.descripcion}>
+  </Articulo>*/
     <div>
     <Header/>
     <h1>hello world!!! :D</h1>
     <h2 className="rojo">This end is coming ( ͡° ͜ʖ ͡°)</h2>
     <h3>Exchange your money to gold ¯\_( ͡° ͜ʖ ͡°)_/¯</h3>
     <li> ̿̿ ̿̿ ̿̿ ̿'̿'\̵͇̿̿\з= ( ▀ ͜͞ʖ▀) =ε/̵͇̿̿/’̿’̿ ̿ ̿̿ ̿̿ ̿̿ </li>
+    <section className="grid tres">
+      {
+        articulos.map(articulo => {
+          return <Articulo
+          titulo={articulo.titulo}
+          imagen={articulo.imagen}
+          descripcion={articulo.descripcion}
+          ></Articulo>
+        })
+      }
+    </section>
     <li> ̿̿ ̿̿ ̿̿ ̿'̿'\̵͇̿̿\з= ( ▀ ͜͞ʖ▀) =ε/̵͇̿̿/’̿’̿ ̿ ̿̿ ̿̿ ̿̿ </li>
     <li> ̿̿ ̿̿ ̿̿ ̿'̿'\̵͇̿̿\з= ( ▀ ͜͞ʖ▀) =ε/̵͇̿̿/’̿’̿ ̿ ̿̿ ̿̿ ̿̿ </li>
     <Articulo
@@ -24,3 +55,5 @@ function App() {
 }
 
 export default App
+
+
---
+++ vite-project2/src/data.ts
@@ -0,0 +1,22 @@
+type Artic = {
+    titulo: string;
+    imagen: string;
+    descripcion: string;
+}
+export const data: Artic[] = [
+    {
+        titulo: "El premohh",
+        imagen: "",
+        descripcion: "No words needed"
+      },
+      {
+        titulo: "Josemi con flow",
+        imagen: "",
+        descripcion: "Who??"
+      },
+      {
+        titulo: "Gun Jaime",
+        imagen: "",
+        descripcion: "Don't know how to write it"
+      }
+]
