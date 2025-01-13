88f60a40506c3675e4e7aeabf5309595cad722d2
Matheus Bond <pachamamajunior@gmail.com>
2025-01-13 09:10:30 +0100

    CAMBIOS 13-01
    :)

--- react/vite-project/src/componentes/Menu.tsx
+++ react/vite-project/src/componentes/Menu.tsx
@@ -3,12 +3,14 @@ import './css/Menu.css'
 function Menu(){
     const inicio = "Home, cambio constante";
     return(
+        <>
         <nav className='menu'>
         <a href="https://www.friv.com/">Free Games</a>
         <a href="https://www.youtube.com/watch?v=lg7sn2qgrwQ">Curious song</a>
         <a href="https://github.com/">Git hub</a>
         <a href="#">{inicio}</a>
         </nav>
+        </>
     )
 }
 export default Menu;
