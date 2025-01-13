8dbc0afe929603e48b8bf3009ff47dc784bb4e04
Matheus Bond <pachamamajunior@gmail.com>
2025-01-13 10:16:07 +0100

    CAMBIOS 13-01
    :)

---
+++ vite-project2/src/App.tsx
@@ -0,0 +1,16 @@
+function App() {
+
+  return (
+    //<div> Puede ser una etiqueta vacía, también
+    <div>
+    <h1>hello world!!! :D</h1>
+    <h2>This end is coming ( ͡° ͜ʖ ͡°)</h2>
+    <h3>Exchange your money to gold ¯\_( ͡° ͜ʖ ͡°)_/¯</h3>
+    <li> ̿̿ ̿̿ ̿̿ ̿'̿'\̵͇̿̿\з= ( ▀ ͜͞ʖ▀) =ε/̵͇̿̿/’̿’̿ ̿ ̿̿ ̿̿ ̿̿ </li>
+    <li> ̿̿ ̿̿ ̿̿ ̿'̿'\̵͇̿̿\з= ( ▀ ͜͞ʖ▀) =ε/̵͇̿̿/’̿’̿ ̿ ̿̿ ̿̿ ̿̿ </li>
+    <li> ̿̿ ̿̿ ̿̿ ̿'̿'\̵͇̿̿\з= ( ▀ ͜͞ʖ▀) =ε/̵͇̿̿/’̿’̿ ̿ ̿̿ ̿̿ ̿̿ </li>
+    </div>
+  )
+}
+
+export default App
