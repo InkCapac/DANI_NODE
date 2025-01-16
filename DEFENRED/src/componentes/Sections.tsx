337e5f3cc6da36e8c06650fb5c6c0527f759a307
Matheus Bond <pachamamajunior@gmail.com>
2025-01-16 10:21:19 +0100

    CAMBIOS 16-01
    :)

--- DEFENRED/src/componentes/Sections.tsx
+++ DEFENRED/src/componentes/Sections.tsx
@@ -7,9 +7,24 @@ function Sections() {
                         <p>Defenred es una organización que nace con el firme propósito de que los defensores y defensoras de derechos humanos puedan desarrollar su labor, y apoyarles de una forma directa en la importante tarea que realizan de construir un mundo más justo. Nuestro objetivo es apoyar su trabajo para el desarrollo justo e igualitario de su entorno.</p>
                     </section>
                     <section>
-
+                        <h1>Casa de respiro</h1>
+                        <p>A diario, la violencia y el hostigamiento que sufren estas personas afecta gravemente a su salud y bienestar. Por ese motivo estamos convencidos de que es necesario para ellos encontrar espacios y tiempos para parar, descansar y recuperarse a nivel físico y emocional. Nuestro proyecto quiere responder a esta necesidad ofreciendo un espacio de respiro y descanso donde los defensores y defensoras puedan realizar un proceso de recuperación y autocuidado, apoyado por un equipo de profesionales expertos en temas de salud (física y emocional)</p>
                     </section>
                     <section>
+                        <h1>¿Qué nos motiva?</h1>
+                        <p>La protección y promoción de los derechos humanos (políticos, civiles, económicos, sociales y culturales) es la base de la libertad, la justicia y la paz. Las personas que trabajan diariamente para que estos derechos se hagan realidad  son piezas clave en aquellos países con políticas más represivas, o en los países embarcados en reformas profundas o que atraviesan períodos de transición. Incluso son imprescindibles en sociedades democráticas, donde suelen ser el principal apoyo de las minorías. Ellas son las  personas defensoras de derechos humanos.
+
+Pero defender los derechos humanos puede convertirse en una amenaza. Muchas sufren un acoso constante, desde amenazas veladas, a hostigamiento e incluso agresiones físicas, a ellas o a sus familiares. El motivo es claro: con su labor ponen en cuestionamiento el poder de aquellos que sustentan su posición privilegiada a través del abuso y las violaciones de los derechos de los demás.
+
+Frente a estas  amenazas las propias personas defensoras generan sus estrategias de protección en su vida diaria, con el objetivo de minimizar el riesgo. Y muchas organizaciones internacionales están abordando formas diversas de apoyar a los defensores y defensoras de derechos humanos para manejar de forma adecuada este riesgo en su día a día, en su trabajo o en sus comunicaciones digitales.
+
+Las amenazas no suelen rebajar la enorme implicación por la justicia que muestran los defensores y defensoras, que les lleva también a mantener un altísimo ritmo de trabajo, con apenas descanso. A esto hay que añadir que en muchas ocasiones los defensores/as y sus organizaciones se encuentran en una situación económica precaria, sin apenas recursos que permitan desarrollar con las suficientes garantías los proyectos que pretenden abordar. Como consecuencia, la falta de tiempo y/o de recursos impide en muchos casos la formación continua, la realización de estudios y capacitaciones, que les permita reflexionar sobre su labor y adquirir nuevos conocimientos para una mejora continua de su trabajo.
+
+La unión de todos estos factores (la amenaza y agresión hacia su persona y sus familiares; un alto nivel de implicación y activismo que les mantiene en la lucha por los derechos humanos a pesar del peligro; y la precariedad económica en que desarrollan su labor) suele conllevar un alto coste para su salud mental y física.
+
+Es necesario apoyar el trabajo de estas personas. Las personas defensoras de derechos humanos son piezas clave para el desarrollo de su entorno de una manera justa e igualitaria. Suelen tener un profundo conocimiento de sus realidades. Y por la influencia en sus comunidades, son los que pueden generar mecanismos de cambio y alternativas para mejorar la situación de derechos humanos.
+
+El objetivo de nuestro grupo es reforzar su labor, atendiendo a algunas de las necesidades señaladas. Si apoyamos a los defensores y defensoras, lo estaremos haciendo a todas las personas y comunidades que se benefician de su trabajo por los derechos humanos. Y estaremos mejorando la situación de derechos humanos de todas ellas.S</p>
                         </section></div>
         </section>
     )
