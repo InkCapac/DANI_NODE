d49010c5df09bc3bc8555499bc55046b31dc32e0
Matheus_Bond <pachamamajunior@gmail.com>
2025-01-19 14:40:02 +0100

    CAMBIOS 19-01
    :)

--- DEFENRED/src/main.tsx
+++ DEFENRED/src/main.tsx
@@ -1,9 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.tsx';
 
-createRoot(document.querySelector('body')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+// Selecciona el contenedor con id "root"
+const container = document.getElementById('root');
+
+if (container) {
+  createRoot(container).render(
+    <StrictMode>
+      <App />
+    </StrictMode>
+  );
+} else {
+  console.error('No se encontró un elemento con id "root". Asegúrate de que exista en el archivo HTML.');
+}
