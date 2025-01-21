57045c90dfdfb06a21e4b7961c855019acce9511
Matheus Bond <pachamamajunior@gmail.com>
2025-01-21 08:52:25 +0100

    CAMBIOS 21-01
    :)

---
+++ DEFENRED/src/componentes/navTab.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+const navTab = () => {
+    return <nav className="grid navbar">
+        <a href="#inicio-index">Inicio</a>
+        <a href="./registro.php">Registrarse</a>
+        <a href=""> </a>
+        <a href="./galeria.php">Galería</a>
+        <a href="">Favoritos</a>
+    </nav>
+};
+export default navTab;
---
+++ DEFENRED/src/linkedPages/inicio.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import navTab from "../componentes/navTab";
+
+const inicio = () => {
+    <h2></h2>
+};
+export default inicio;
