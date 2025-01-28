4b8095ca602ac8b6e6ad2ccffedd8e0619ba67d9
Matheus Bond <pachamamajunior@gmail.com>
2025-01-28 08:48:55 +0100

    CAMBIOS 28-01
    :)

--- DEFENRED/src/App.tsx
+++ DEFENRED/src/App.tsx
@@ -1,63 +1,22 @@
-//import React from "react";
+import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./index";
-//import {data} from 'data';
-import {useRef} from 'react';
-const App = () => {
-  const nombre = useRef<HTMLInputElement>(null);
-  const apellido = useRef<HTMLInputElement>(null);
-  const apellido2 = useRef<HTMLInputElement>(null);
-  const edad = useRef<HTMLInputElement>(null);
-  const correo = useRef<HTMLInputElement>(null);
-  const pass = useRef<HTMLInputElement>(null);
+import Formulario from "./componentes/Formulario"; // Importa el componente Formulario
 
-  const enviar = (event: any) => {
-    event?.preventDefault();
-    const usuario = {
-    nombre: nombre.current?.value,
-    apellido: apellido.current?.value,
-    apellido2: apellido2.current?.value,
-    edad: edad.current?.value,
-    correo: correo.current?.value,
-    pass: pass.current?.value
-    }
-    console.log(usuario);
-    fetch("http://localhost:8080/insertarUsuario",
-      {
-        method: "POST",
-        headers: {
-          'Content-type': 'application/json'
-        },
-        body: JSON.stringify(usuario),
-      }
-    )
-  }
+const App: React.FC = () => {
   return (
     <section>
-      <form onSubmit={enviar}>
-        <label>Nombre</label>
-        <input type='text' ref={nombre} />
-        <label>Apellido</label>
-        <input type='text' ref={apellido} />
-        <label>Apellido2</label>
-        <input type='text' ref={apellido2} />
-        <label>Edad</label>
-        <input type='number' ref={edad} />
-        <label>Correo</label>
-        <input type='email' ref={correo} />
-        <label>Contraseña</label>
-        <input type='password' ref={pass} />
-        <button>Enviar</button>
-      </form>
+      {/* Navegación de rutas */}
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Index />}>
-          </Route>
-          <Route
-            path="/contacto"
-            element={<h1>Contacta con nosotros</h1>}
-          >
-          </Route>
+          {/* Ruta principal */}
+          <Route path="/" element={<Index />} />
+          
+          {/* Ruta de contacto */}
+          <Route path="/contacto" element={<h1>Contacta con nosotros</h1>} />
+          
+          {/* Ruta del formulario */}
+          <Route path="/formulario" element={<Formulario />} />
         </Routes>
       </BrowserRouter>
     </section>
--- DEFENRED/src/index.tsx
+++ DEFENRED/src/index.tsx
@@ -1,11 +1,16 @@
 import Inicio from "./linkedPages/inicio";
 import Navtab from "./componentes/navTab";
 import Slider from "./componentes/Slider";
+import Formulario from "./componentes/Formulario";
 import './App.css'
 const Index = () => {
     return (
       //<BrowserRouter>
       <div>
+        <Inicio></Inicio>
+        <Navtab></Navtab>
+        <Slider></Slider>
+        <Formulario></Formulario>
       </div>
     );
   };
