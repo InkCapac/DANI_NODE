1d36b5bda2b537ec75dfdedd7e02111dd6c64f82
Matheus Bond <pachamamajunior@gmail.com>
2025-01-31 10:04:19 +0100

    CAMBIOS 31-01
    :)

--- DEFENRED/src/linkedPages/inicio.tsx
+++ DEFENRED/src/linkedPages/inicio.tsx
@@ -1,10 +1,11 @@
 import React from "react";
-import "../componentes/css/inicioStyle.css"
+import "./css_pages/inicioStyle.css"
 
 const Inicio = () => {
     return (
         <div>
-            <h2>DEFENRED</h2>
+            <h2 className="title-inicio">DEFENRED</h2>
+            <h3>"Defenred-Red de Apoyo para Defensores de Derechos Humanos"</h3>
             <p className="parrafo1-inicio">
                 Defenred es una organización comprometida con el apoyo y la protección de defensores de
                 derechos humanos que enfrentan riesgos en sus países de origen. A través de programas de
