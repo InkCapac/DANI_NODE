9a1261a9673a551ad6a61d60afdc63d14ad2abbe
Matheus Bond <pachamamajunior@gmail.com>
2025-01-31 10:33:45 +0100

    CAMBIOS 31-01
    :)

--- DEFENRED/src/linkedPages/inicio.tsx
+++ DEFENRED/src/linkedPages/inicio.tsx
@@ -5,9 +5,9 @@ const Inicio = () => {
     return (
         <div>
             <h2 className="title-inicio">DEFENRED</h2>
-            <h3>"Defenred-Red de Apoyo para Defensores de Derechos Humanos"</h3>
+            <h3 className="meta-info">"Defenred-Red de Apoyo para Defensores de Derechos Humanos"</h3>
             <div className="cta-container">
-                <button className="cta-unete">Únete a la red</button>
+                <button className="cta-unete">Únete a la red!</button>
             </div>
             <p className="parrafo1-inicio">
                 <span className="keywords">Defenred</span> es una organización comprometida con el apoyo y la protección de defensores de <span className="keywords">Defenred</span> que enfrentan riesgos en sus países de origen. A través de programas de
