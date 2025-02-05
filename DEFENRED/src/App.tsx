9fd8eda1b2c23fc4b2fff823da8b903371012b84
Matheus Bond <pachamamajunior@gmail.com>
2025-02-05 08:48:09 +0100

    CAMBIOS 05-02
    :)

--- DEFENRED/src/App.tsx
+++ DEFENRED/src/App.tsx
@@ -1,15 +1,17 @@
-import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React from "react";
 import Navtab from "./componentes/navTab";
 import Index from "./index";
 import Contacto from "./linkedPages/contacto";
 import QueHacemos from "./linkedPages/queHacemos";
 import Defensoras from "./linkedPages/defensoras";
+import { ParallaxProvider } from "react-scroll-parallax";
 
 const App: React.FC = () => {
   return (
     
     <section>
+      <ParallaxProvider>
       {/* Barra de navegación */}
       <BrowserRouter>
         <Navtab /> {/* Incluye la barra de navegación en todas las páginas */}
@@ -23,6 +25,7 @@ const App: React.FC = () => {
           <Route path="/contacto" element={<Contacto />} />
         </Routes>
       </BrowserRouter>
+      </ParallaxProvider>
     </section>
     
   );
