15ab3de589047814114fbbb210db16537fc6a539
Matheus Bond <pachamamajunior@gmail.com>
2025-02-05 12:56:58 +0100

    CAMBIOS 05-02
    :)

--- DEFENRED/src/linkedPages/inicio.tsx
+++ DEFENRED/src/linkedPages/inicio.tsx
@@ -1,7 +1,7 @@
 import Footer from "../componentes/Footer"
 import "../linkedPages/css_pages/inicioStyle.css";
 import "./css_pages/inicioStyle.css"
-import reunionImage from "../assets/img/reunion_defenred.jpg"
+import streetImage from "../assets/img/street_defenred.jpg"
 import { useParallax } from "react-scroll-parallax";
 
 const Inicio = () => {
@@ -16,6 +16,7 @@ const Inicio = () => {
                 <button className="cta-unete">Únete a la red!</button>
             </div>
             <div className="parrafo1-inicio">
+            <img src={streetImage} alt="Vistas de Defenred" />
             <p>
                 <span className="keywords">Defenred</span> es una organización comprometida con el apoyo y la protección de defensores de <span className="keywords">Defenred</span> que enfrentan riesgos en sus países de origen. A través de programas de
                 refugio, formación, autocuidado y redes de apoyo, Defenred brinda a estos activistas un espacio
@@ -25,7 +26,6 @@ const Inicio = () => {
                 justicia y los derechos humanos en contextos de alta vulnerabilidad, ofreciéndoles el respaldo
                 necesario para que puedan continuar con su labor esencial.
             </p>
-            <img src={reunionImage} alt="Reunión Defenred" />
             </div>
             <Footer />
         </div>
