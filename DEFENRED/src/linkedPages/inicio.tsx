25c902efb11ce4733559844ac1c2cf3d2c4b0e99
Matheus Bond <pachamamajunior@gmail.com>
2025-02-10 09:51:09 +0100

    CAMBIOS 10-02
    :)

--- DEFENRED/src/linkedPages/inicio.tsx
+++ DEFENRED/src/linkedPages/inicio.tsx
@@ -1,7 +1,9 @@
 import Footer from "../componentes/Footer"
 import "../linkedPages/css_pages/inicioStyle.css";
-import "./css_pages/inicioStyle.css"
-import streetImage from "../assets/img/street_defenred.jpg"
+import "./css_pages/inicioStyle.css";
+import streetImage from "../assets/img/street_defenred.jpg";
+import streetImage2 from "../assets/img/street2_defenred.jpg";
+import casaRespiro from "../assets/img/señoraLeyendo_defenred.jpg";
 import { useParallax } from "react-scroll-parallax";
 
 const Inicio = () => {
@@ -16,27 +18,29 @@ const Inicio = () => {
                 <button className="cta-unete">Únete a la red!</button>
             </div>
             <div className="parrafo1-inicio">
-            <img src={streetImage} alt="Vistas de Defenred" />
-            <p>
-                <span className="keywords">Defenred</span> es una organización comprometida con el apoyo y la protección de defensores de <span className="keywords">Defenred</span> que enfrentan riesgos en sus países de origen. A través de programas de
-                refugio, formación, autocuidado y redes de apoyo, Defenred brinda a estos activistas un espacio
-                seguro para recuperar fuerzas, adquirir nuevas herramientas y conectarse con aliados
-                internacionales.
-                Nuestra misión es fortalecer el bienestar, la seguridad y la resiliencia de quienes luchan por la
-                justicia y los derechos humanos en contextos de alta vulnerabilidad, ofreciéndoles el respaldo
-                necesario para que puedan continuar con su labor esencial.
-            </p>
+                <img src={streetImage} alt="Vistas de Defenred" />
+                <p>
+                    <span className="keywords">Defenred</span> es una organización comprometida con el apoyo y la protección de defensores de <span className="keywords">Defenred</span> que enfrentan riesgos en sus países de origen. A través de programas de
+                    refugio, formación, autocuidado y redes de apoyo, Defenred brinda a estos activistas un espacio
+                    seguro para recuperar fuerzas, adquirir nuevas herramientas y conectarse con aliados
+                    internacionales.
+                    Nuestra misión es fortalecer el bienestar, la seguridad y la resiliencia de quienes luchan por la
+                    justicia y los derechos humanos en contextos de alta vulnerabilidad, ofreciéndoles el respaldo
+                    necesario para que puedan continuar con su labor esencial.
+                </p>
             </div>
-            <div className="parrafo1-inicio">
-            <p>
-                <span className="keywords">Defenred</span> es una organización comprometida con el apoyo y la protección de defensores de <span className="keywords">Defenred</span> que enfrentan riesgos en sus países de origen. A través de programas de
-                refugio, formación, autocuidado y redes de apoyo, Defenred brinda a estos activistas un espacio
-                seguro para recuperar fuerzas, adquirir nuevas herramientas y conectarse con aliados
-                internacionales.
-                Nuestra misión es fortalecer el bienestar, la seguridad y la resiliencia de quienes luchan por la
-                justicia y los derechos humanos en contextos de alta vulnerabilidad, ofreciéndoles el respaldo
-                necesario para que puedan continuar con su labor esencial.
-            </p>
+            <div className="casaRespiro-inicio">
+                <p className="casaRespiro-parrafo1">
+                    <span className="keywords">Defenred</span> lanza <span className="keywords">“Casa de Respiro”</span>, un programa en La Cabrera, Madrid, que brinda a
+                    defensores de <span className="keywords">derechos humanos</span> un espacio seguro para el autocuidado y el <span className="keywords">bienestar</span>.
+                    Durante 4-5 semanas, los participantes pueden desconectar y recibir apoyo psicológico,
+                    prácticas de trabajo corporal y actividades de ocio. El objetivo es que regresen con herramientas
+                    de autocuidado que fortalezcan su <span className="keywords">bienestar</span> y sostenibilidad en su labor activista.
+                </p>
+                <div className="casaRespiro-inicio-image">
+                    <img src={casaRespiro} alt="Casa de Respiro" />
+                    <button className="cta-casaRespiro">Casa de Respiro</button>
+                </div>
             </div>
             <Footer />
         </div>
