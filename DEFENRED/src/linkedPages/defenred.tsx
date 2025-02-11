cc7b931c6e2757b7367e656841627c156db7ed1e
Matheus Bond <pachamamajunior@gmail.com>
2025-02-11 10:42:26 +0100

    CAMBIOS 11-02
    :)

--- DEFENRED/src/linkedPages/defenred.tsx
+++ DEFENRED/src/linkedPages/defenred.tsx
@@ -3,9 +3,13 @@ import { Link, Outlet } from "react-router-dom";
 import PorqueProyecto from "../linkedPages/subLinked/porqueProyecto";
 import Footer from "../componentes/Footer";
 import fountainDefenred from "../assets/img/fountain_defenred.jpg";
-import "./css_pages/defenred.css"
+import "./css_pages/defenred.css";
+import { useParallax } from "react-scroll-parallax";
 
 const Defenred = () => {
+  const parallax = useParallax<HTMLDivElement>({
+    speed: 50
+  });
   return (
     <section className="seccion-defenred-inicio">
       <div className="defenred-gallery">
@@ -13,7 +17,9 @@ const Defenred = () => {
           <img className="gallery-item active" src={fountainDefenred} alt="Imagen" />
         </div>
       </div>
-      <h2>¿Por qué elegimos este proyecto?</h2>
+      <div className="cta-container" ref={parallax.ref}>
+        <button className="cta-porque">¿Por qué elegimos este proyecto?</button>
+      </div>
       <nav>
         <ul>
           <li>
