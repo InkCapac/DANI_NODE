309eb420a8698794f2ac3f15e1600a372a90eef8
Rodrigo Ramírez López <cientoundalmatas693@gmail.com>
2025-02-15 21:00:20 +0100

    Update SliderIMGS.tsx

--- DEFENRED/src/componentes/SliderIMGS.tsx
+++ DEFENRED/src/componentes/SliderIMGS.tsx
@@ -16,9 +16,26 @@ const images = [
     thumbnail: "src/assets/img/salón_defenred.webp",
   },
 ];
+
 class MyGallery extends React.Component {
   render() {
-    return <ImageGallery items={images} showBullets={true} />;
+    return (
+      <div style={{ width: "100vw", height: "100vh" }}>
+        <ImageGallery
+          items={images}
+          showBullets={true}
+          autoPlay={true}
+          renderItem={(item) => (
+            <img
+              src={item.original}
+              alt=""
+              style={{ width: "100%", height: "100vh" }}
+            />
+          )}
+        />
+      </div>
+    );
   }
 }
+
 export default MyGallery;
