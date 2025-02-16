6887f68f92bddba90dec20011f6b5f36b93ca9ec
Rodrigo Ramírez López <cientoundalmatas693@gmail.com>
2025-02-16 20:07:38 +0100

    Elimina el componente Navtab y simplifica el renderizado en el componente Index

--- DEFENRED/src/index.tsx
+++ DEFENRED/src/index.tsx
@@ -1,14 +1,11 @@
 import Inicio from "./linkedPages/inicio";
-import Navtab from "./componentes/navTab";
-/*import Slider from "./componentes/Slider";*/
 import './App.css'
 const Index = () => {
     return (
       //<BrowserRouter>
-      <div>
-        <Navtab></Navtab>
-        <Inicio></Inicio>
-      </div>
+     <div>
+      <Inicio />
+     </div>
     );
   };
   
