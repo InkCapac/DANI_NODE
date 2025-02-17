25c55f6d7fddaf97f5694f6a97b4c649e9df5909
Rodrigo Ramírez López <cientoundalmatas693@gmail.com>
2025-02-17 13:18:57 +0100

    Agregar estilos y contenido a la página "Que Hacemos", incluyendo un nuevo fondo y una imagen SVG.

--- DEFENRED/src/linkedPages/queHacemos.tsx
+++ DEFENRED/src/linkedPages/queHacemos.tsx
@@ -1,15 +1,23 @@
-import Publicaciones from "./subLinked/Publicaciones";
 import Footer from "../componentes/Footer";
 import "./css_pages/queHacemos.css";
 
 const QueHacemos = () => {
   return (
-    <div> {/* Considera eliminar el id="root" si no es necesario */}
+    <div>
+      {" "}
+      {/* Considera eliminar el id="root" si no es necesario */}
       <main>
         <section>
-          <img className="casaRespiro" src="src/assets/img/casa_respiro.webp" alt="Imagen casa respiro"/>
+          <img
+            className="casaRespiro"
+            src="src/assets/img/casa_respiro.webp"
+            alt="Imagen casa respiro"
+          />
           <h1 className="textoNaranja">Casa de Respiro</h1>
-          <p className="textoBlanco">Programa de respiro y formación sobre autocuidado</p>
+          <p className="textoBlanco">
+            Programa de respiro y formación sobre autocuidado
+          </p>
+          <img className="olas" src="src/assets/img/descarga.svg" alt="" />
         </section>
       </main>
       <Footer />
