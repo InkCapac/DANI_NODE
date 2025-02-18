5c243cede4437310f85d93fa295782490f0e1f17
Matheus Bond <pachamamajunior@gmail.com>
2025-02-18 09:27:13 +0100

    CAMBIOS 18-02
    :)

--- DEFENRED/src/componentes/Footer.tsx
+++ DEFENRED/src/componentes/Footer.tsx
@@ -6,23 +6,23 @@ const Footer = () => {
             <footer>
                 <div className="footerContainer">
                     <div className="socialIcons">
-                        <a href="https://www.facebook.com/marcaPERU.lat">
+                        <a href="https://www.facebook.com/Defenred/?locale=es_ES">
                             <i className="fa-brands fa-facebook"></i>
                         </a>
-                        <a href="https://www.instagram.com/peru/">
+                        <a href="https://www.instagram.com/pbi_ee?igsh=MXhnaTFxbG4wZHoxNg==">
                             <i className="fa-brands fa-instagram"></i>
                         </a>
-                        <a href="https://x.com/marcaPERU?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor">
+                        <a href="https://x.com/defenred?t=sQtzzxouuO91MNuNcM0Elw&s=03">
                             <i className="fa-brands fa-twitter"></i>
                         </a>
-                        <a href="https://www.youtube.com/@marcaperu">
+                        <a href="https://www.youtube.com/@DEFENRED">
                             <i className="fa-brands fa-youtube"></i>
                         </a>
                     </div>
                     {/* Nueva sección de enlaces */}
            
 
-                    <p className="footer-p">© 2024 | Defenred. All rights reserved.</p>
+                    <p className="footer-p">© 2025 | Defenred. All rights reserved.</p>
                 </div>
             </footer>
         </section>
