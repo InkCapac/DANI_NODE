9d47ad1c4674dcb3cdf81b08722089aadc8b20ba
Matheus Bond <pachamamajunior@gmail.com>
2025-02-20 13:34:42 +0100

    CAMBIOS 20-02
    :)

--- DEFENRED/src/componentes/Footer.tsx
+++ DEFENRED/src/componentes/Footer.tsx
@@ -1,27 +1,41 @@
+import defenredFooter from "../assets/img/defenred_footer.webp";
+import "./css/footer.css";
 
-import "./css/footer.css"
 const Footer = () => {
     return (
         <section>
             <footer>
                 <div className="footerContainer">
-                    <div className="socialIcons">
-                        <a href="https://www.facebook.com/Defenred/?locale=es_ES" aria-label="Facebook">
-                            <i className="fa-brands fa-facebook"></i>
-                        </a>
-                        <a href="https://www.instagram.com/pbi_ee?igsh=MXhnaTFxbG4wZHoxNg==" aria-label="Instagram">
-                            <i className="fa-brands fa-instagram"></i>
-                        </a>
-                        <a href="https://x.com/defenred?t=sQtzzxouuO91MNuNcM0Elw&s=03" aria-label="Twitter">
-                            <i className="fa-brands fa-twitter"></i>
-                        </a>
-                        <a href="https://www.youtube.com/@DEFENRED" aria-label="Youtube">
-                            <i className="fa-brands fa-youtube"></i>
-                        </a>
+                    {/* Contenedor para la imagen y el contenido */}
+                    <div className="footerContent">
+                        {/* Imagen a la izquierda */}
+                        <div className="footerImage">
+                            <img src={defenredFooter} alt="Footer Image" />
+                        </div>
+                        {/* Contenido a la derecha */}
+                        <div className="footerText">
+                            <div className="socialIcons">
+                                <a href="https://www.facebook.com/Defenred/?locale=es_ES" aria-label="Facebook">
+                                    <i className="fa-brands fa-facebook"></i>
+                                </a>
+                                <a href="https://www.instagram.com/pbi_ee?igsh=MXhnaTFxbG4wZHoxNg==" aria-label="Instagram">
+                                    <i className="fa-brands fa-instagram"></i>
+                                </a>
+                                <a href="https://x.com/defenred?t=sQtzzxouuO91MNuNcM0Elw&s=03" aria-label="Twitter">
+                                    <i className="fa-brands fa-twitter"></i>
+                                </a>
+                                <a href="https://www.youtube.com/@DEFENRED" aria-label="Youtube">
+                                    <i className="fa-brands fa-youtube"></i>
+                                </a>
+                            </div>
+                            <p className="footer-disclaimer">
+                                La protección y promoción de los derechos humanos políticos, civiles, económicos, sociales y culturales es la base de la libertad, la justicia y la paz. Las personas que trabajan diariamente para que estos derechos se hagan realidad son piezas clave en aquellos países con políticas más represivas, o en los países embarcados en reformas profundas o que atraviesan períodos de transición.
+                            </p>
+                            <p className="footer-p">
+                                <span className="footer-year">© 2025 | Defenred - </span><span className="footer-links">Política de privacidad</span> - <span className="footer-links">Aviso Legal</span>. All rights reserved.
+                            </p>
+                        </div>
                     </div>
-                    <p className="footer-disclaimer">La protección y promoción de los derechos humanos (políticos, civiles, económicos, sociales y culturales) es la base de la libertad, la justicia y la paz. Las personas que trabajan diariamente para que estos derechos se hagan realidad  son piezas clave en aquellos países con políticas más represivas, o en los países embarcados en reformas profundas o que atraviesan períodos de transición.</p>
-                    {/* Nueva sección de enlaces */}
-                    <p className="footer-p">© 2025 | Defenred- Política de privacidad - Aviso Legal. All rights reserved.</p>
                 </div>
             </footer>
         </section>
--- DEFENRED/src/linkedPages/subLinked/sections_defensoras.tsx
+++ DEFENRED/src/linkedPages/subLinked/sections_defensoras.tsx
@@ -25,11 +25,11 @@ const sec_defensoras = () => {
                             <span className="section-defensoras-quote">Las personas <span className="keywords-defensoras">defensoras
                             </span> que llegan a la Casa de Respiro suelen provenir de entornos difíciles y, a menudo, peligrosos, lo que afecta su salud física, emocional y mental.</span> Es importante que dediquen un tiempo para cuidar de sí mismos, así podrán procesar lo que han vivido y fortalecer su capacidad de resistir. <span className="section-defensoras-quote">La mayoría de estos activistas cuentan con el apoyo de importantes organizaciones y colectivos en sus países, como la IMD, Caminando Fronteras o la Red Nacional de Mujeres Defensoras de Honduras.</span> Estos grupos no solo brindan apoyo a los <span className="keywords-defensoras">defensores
                             </span>, sino que también trabajan en conjunto con la Casa para garantizar que el cuidado recibido beneficie a las comunidades de origen.</p>
-                        <img src={defenredTrain} alt="Reunión Defenred" />
+                        <img  className="defenredTrain" src={defenredTrain} alt="Reunión Defenred" />
                     </div>
                     <div className="section-defensoras">
-                        <img src={defenredTeam} alt="Reunión Defenred" />
-                        <p>La <span className="keywords-defensoras">Casa de Respiro</span> planea compartir más experiencias de <span className="keywords-defensoras">defensores</span> de derechos humanos en su página web, para dar visibilidad a sus historias y proyectos, y concienciar al público sobre la importancia de esta labor colectiva. Este lugar nos recuerda que la lucha por los <span className="keywords-defensoras">derechos humanos</span> es más efectiva cuando se realiza de forma colectiva. Aquí, cada persona y grupo pueden seguir fortaleciendo su trabajo en defensa de los <span className="keywords-defensoras">derechos</span>  fundamentales en sus comunidades. 
+                        <img className="defenredTeam" src={defenredTeam} alt="Reunión Defenred" />
+                        <p>La <span className="keywords-defensoras">Casa de Respiro</span> planea compartir más experiencias de <span className="keywords-defensoras">defensores</span> de derechos humanos en su página web, para dar visibilidad a sus historias y proyectos, y concienciar al público sobre la importancia de esta labor colectiva. Este lugar nos recuerda que la lucha por los <span className="keywords-defensoras">derechos humanos</span> es más efectiva cuando se realiza de forma colectiva. <span className="section-defensoras-quote">Aquí, cada persona y grupo pueden seguir fortaleciendo su trabajo en defensa de los <span className="keywords-defensoras">derechos</span>  fundamentales en sus comunidades.</span>
                         </p>
                     </div>
                     </div>
