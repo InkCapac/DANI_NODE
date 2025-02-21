9caa45bb7cdf3b4ed07e70b48d213a1a72b08be6
Matheus Bond <pachamamajunior@gmail.com>
2025-02-21 11:09:56 +0100

    CAMBIOS 21-02
    :)

--- DEFENRED/src/linkedPages/subLinked/casaRespiro.tsx
+++ DEFENRED/src/linkedPages/subLinked/casaRespiro.tsx
@@ -11,8 +11,7 @@ const casaRespiro = () => {
       </div>
       <div className="casaRespiro">
         <p className="casaRespiro-title">Casa de Respiro</p>
-        <p className="casaRespiro-parrafo1">La casa del respiro es el corazón de<span className="keywords">Defenred</span>un lugar para que aquellos <span className="keywords">activistas</span>  que quieran<span className="keywords">descansar</span> y tomarse un pequeño<span className="keywords">descanso</span>de su actividad principal. A través de este espacio y de las actividades que ofrecemos, nuestro equipo es capaz de<span className="keywords"> ayudar </span>generando un estado de relajación y de recuperación del desgaste tanto físico como mental generado por sus diferentes situaciones sociales.
-        </p>
+        <p className="casaRespiro-parrafo1">La casa del respiro es el corazón de<span className="keywords">Defenred</span>un lugar para que aquellos <span className="keywords">activistas</span>  que quieran<span className="keywords">descansar</span> y tomarse un pequeño<span className="keywords">descanso</span>de su actividad principal. A través de este espacio y de las actividades que ofrecemos, nuestro equipo es capaz de<span className="keywords"> ayudar </span>generando un estado de relajación y de recuperación del desgaste tanto físico como mental generado por sus diferentes situaciones sociales.</p>
         <p className="casaRespiro-parrafo1-quote"></p>
       </div>
     </section>
